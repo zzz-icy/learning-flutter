e591194b0beec849c1f518d14a3314a0abc287cc
Hannah Z <eveline1129@163.com>
2024-11-04 22:02:07 -0600

    added more widgets and configuring

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,19 @@ import 'package:flutter/material.dart';
 void main() {
   // the const here help improve performance, if this will be reused
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        // backgroundColor: Colors.deepPurple,
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Color.fromARGB(255, 26, 2, 80),
+              Color.fromARGB(255, 23, 6, 47)
+            ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+          ),
+          child: const Center(
+            child: Text('Hello World!'),
+          ),
         ),
       ),
     ),
