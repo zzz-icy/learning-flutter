279e60fce63bdd0bda8287853daaacac107adee8
Hannah Z <eveline1129@163.com>
2024-11-05 11:33:03 -0600

    configurable widget

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -33,7 +33,7 @@ class GradientContainer extends StatelessWidget {
         ], begin: startAlignment, end: endAlignment),
       ),
       child: const Center(
-        child: StyledText(),
+        child: StyledText('Hello World!'),
       ),
     );
   }
--- lib/styled_text.dart
+++ lib/styled_text.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
 
 class StyledText extends StatelessWidget {
-  const StyledText({super.key});
+  // positional arguments , named arguments
+  // const StyledText(String text, {super.key}) : outputText = text;
+  const StyledText(this.text, {super.key});
+//  keyword "this" is used inside of classes, to refer to the class itself or to refer to the object
+// that will be built based on the class.
+// String outputText;
+// by using this.text,  you don't just wanna accept this positional argument,
+// but that instead, it should also automatically be mapped
+//to a class variable with the same name."
+
+  // String text;
+// this will only be set once by this argument that is received, and will not be changed thereafter.
+  final String
+      text; // with this the const can be re-added to the constructor function
+  //because since text will never change internally,
+// this is guaranteed to always be the same object
+// once it has been initialized.
+
   @override
   Widget build(BuildContext context) {
-    return const Text(
-      'Hello World!',
-      style: TextStyle(color: Colors.white, fontSize: 28),
+    return Text(
+      text,
+      style: const TextStyle(color: Colors.white, fontSize: 28),
     );
   }
 }
